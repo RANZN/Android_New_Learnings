fbf25120cd2ea72cf8313624a4a1103fd209565c
ranjan_96 <prakash.ranjan@nium.com>
2022-12-21 12:33:58 +0530

    JUST making it master branch

--- app/src/main/java/dev/ranjan/androidnewlearnings/MainActivity.kt
+++ app/src/main/java/dev/ranjan/androidnewlearnings/MainActivity.kt
@@ -2,11 +2,6 @@ package dev.ranjan.androidnewlearnings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dev.ranjan.androidnewlearnings.encrypted_storage.EncryptedRoomDatabase
-import dev.ranjan.androidnewlearnings.encrypted_storage.room.DatabaseTable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val database = EncryptedRoomDatabase.buildDatabase("ranjan", this)
-        val dao = database.getDao()
-
-        CoroutineScope(Dispatchers.IO).launch {
-            dao.insertData(DatabaseTable("Ranjan"))
-        }
     }
 }
