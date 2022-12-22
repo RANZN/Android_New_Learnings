42fee4f0df49b39b96390d7744664bc060e57c0c
ranjan_96 <prakash.ranjan@nium.com>
2022-12-22 09:32:19 +0530

    enum test

--- app/src/main/java/dev/ranjan/androidnewlearnings/MainActivity.kt
+++ app/src/main/java/dev/ranjan/androidnewlearnings/MainActivity.kt
@@ -1,6 +1,8 @@
 package dev.ranjan.androidnewlearnings
 
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        Log.d("TAG", "onCreate: ${this.getString(Data.MALE.gender)}")
     }
 }
+
+enum class Data(val id: Int,@StringRes val gender: Int) {
+    MALE(id = 1, R.string.male),
+    FEMALE(2, R.string.female),
+    TRANSGENDER(3, R.string.trans),
+    OTHER(id = 4, gender = R.string.other),
+}
+
